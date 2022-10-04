1fcbc7802efafcab856b2e3502729ea2b64e15f8
Rachmat Ardico Perdana <rahmatardiko99@gmail.com>
2022-10-04 14:10:55 +0700

    Database Connection

---
+++ app/Config/Database.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Matt\Php\Web\Login\Config;
+
+use PDO;
+
+class Database
+{
+    private static ?PDO $pdo = null;
+
+    public static function getConnection(string $env = "test"):PDO
+    {
+        if (self::$pdo == null) {
+            // create new pdo
+            require_once __DIR__ . '/../../config/Database.php';
+            $config = getDatabaseConfig();
+            self::$pdo = new PDO(
+                $config['database'][$env]['url'],
+                $config['database'][$env]['username'],
+                $config['database'][$env]['password'],
+            );
+        }
+
+        return self::$pdo;
+    }
+}
