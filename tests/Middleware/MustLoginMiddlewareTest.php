8715b1dec470e766213107fad55b9599fee4f214
Rachmat Ardico Perdana <rahmatardiko99@gmail.com>
2022-10-06 15:38:08 +0700

    Unit Test Session Middleware

---
+++ tests/Middleware/MustLoginMiddlewareTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Matt\Php\Web\Login\App
+{
+    function header(string $value)
+    {
+        echo $value;
+    }
+}
+
+namespace Matt\Php\Web\Login\Middleware
+{
+
+    use Matt\Php\Web\Login\Config\Database;
+    use Matt\Php\Web\Login\Domain\Session;
+    use Matt\Php\Web\Login\Domain\User;
+    use Matt\Php\Web\Login\Repository\SessionRepository;
+    use Matt\Php\Web\Login\Repository\UserRepository;
+    use Matt\Php\Web\Login\Service\SessionService;
+    use PHPUnit\Framework\TestCase;
+
+    class MustLoginMiddlewareTest extends TestCase
+    {
+        private MustLoginMiddleware $middleware;
+        private UserRepository $userRepository;
+        private SessionRepository $sessionRepository;
+
+        protected function setUp(): void
+        {
+            $this->middleware = new MustLoginMiddleware();
+            putenv("mode=test");
+
+            $this->userRepository = new UserRepository(Database::getConnection());
+            $this->sessionRepository = new SessionRepository(Database::getConnection());
+
+            $this->sessionRepository->deleteAll();
+            $this->userRepository->deleteAll();
+        }
+
+        public function testBeforeGuest()
+        {
+            $this->middleware->before();
+
+            $this->expectOutputRegex("[Location: /users/login]");
+        }
+
+        public function testBeforeLoginUser()
+        {
+            $user = new User();
+            $user->id = "matt";
+            $user->name = "rachmat";
+            $user->password = "password";
+            $this->userRepository->save($user);
+
+            $session = new Session();
+            $session->id = uniqid();
+            $session->userId = $user->id;
+            $this->sessionRepository->save($session);
+
+            $_COOKIE[SessionService::$COOKIE_NAME] = $session->id;
+
+            $this->middleware->before();
+            $this->expectOutputString("");
+        }
+    }
+}
